2dd17b337773f84338e864abe8fb68c3474c9469
Ankit <ankit.kumar.jsr@gmail.com>
2021-02-15 16:43:42 +0800

    Arrays updated

---
+++ src/ds/arrays_and_strings/is_unique.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func isUnique(s string) bool {
+
+	var strMap map[rune]bool = make(map[rune]bool)
+	for _, c := range s {
+		_, ok := strMap[c]
+		if ok {
+			return false
+		}
+		strMap[c] = true
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isUnique("ankit"))
+	fmt.Println(isUnique("aankit"))
+}
